bb5658dff9e6a889013fe3b21e9a0db3d575ac50
imam-hossain-wd <zunayedemon6t9@gmail.com>
2024-06-11 14:25:27 +0600

    user crud operation complete

--- index.js
+++ index.js
@@ -28,33 +28,57 @@ const run = async () => {
 
     try {
 
-        //all get route from database
+        // user operation
 
-        app.get("/user", async (req, res) => {
-            const query = {};
-            const result = await userCollection.find(query).toArray();
-            res.send(result);
-        });
-
-        app.get("/agent", async (req, res) => {
-            const query = {};
-            const result = await agentCollection.find(query).toArray();
-            res.send(result);
-        });
-        app.get("/customer-service", async (req, res) => {
-            const query = {};
-            const result = await customerServiceCollection.find(query).toArray();
-            res.send(result);
-        });
-
-        ///all post route from database
-        
         app.post("/user", async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
             res.send(result);
         });
 
+        app.get("/users", async (req, res) => {
+            const query = {};
+            const result = await userCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get("/user/:id", async (req, res) => {
+            const userId = req.params.id;
+            const query = { user_id: userId };
+            const result = await userCollection.find(query).toArray();
+            res.send(result);
+        });
+
+
+        app.get("/agents", async (req, res) => {
+            const query = {};
+            const result = await agentCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get("/agent", async (req, res) => {
+            const agentId = req.body.agent_id;
+            const query = { agent_id: agentId };
+            const result = await agentCollection.find(query).toArray();
+            res.send(result);
+        });
+
+
+        app.get("/customer-services", async (req, res) => {
+            const query = {};
+            const result = await customerServiceCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get("/customer-service", async (req, res) => {
+            const id = req.body.id_no;
+            const query = { id_no: id };
+            const result = await customerServiceCollection.find(query).toArray();
+            res.send(result);
+        });
+
+      
+        
         app.post("/agent", async (req, res) => {
             const user = req.body;
             const result = await agentCollection.insertOne(user);
@@ -64,6 +88,77 @@ const run = async () => {
         app.post("/customer-service", async (req, res) => {
             const user = req.body;
             const result = await customerServiceCollection.insertOne(user);
+            res.send(result);
+        });
+
+        // Update User
+        app.patch("/user/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const query = { user_id: id };
+            const updateDoc = {
+                $set: updatedUser,
+                };
+                const result = await userCollection.updateOne(query, updateDoc);
+            res.send(result);
+        });
+
+        app.delete("/user/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        // Update Agent
+        app.patch("/agent/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedAgent = req.body;
+            const query = { agent_id: id };
+            const updateDoc = {
+                $set: updatedAgent,
+            };
+            const result = await agentCollection.updateOne(query, updateDoc);
+            res.send(result);
+        });
+
+        // Update Customer Service
+        app.patch("/customer-service/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedCustomerService = req.body;
+            const query = { id_no: id.toString() };
+            const updateDoc = {
+                $set: updatedCustomerService,
+            };
+            const result = await customerServiceCollection.updateOne(query, updateDoc);
+
+            console.log(result, 'result');
+            
+            if (result.matchedCount === 0) {
+                return res.status(404).send({ message: "Customer service record not found" });
+            }
+            
+            res.send(result);
+        });
+        
+
+        // Delete User
+    
+
+        // Delete Agent
+        app.delete("/agent/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await agentCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // Delete Customer Service
+        app.delete("/customer-service/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await customerServiceCollection.deleteOne(query);
             res.send(result);
         });
 
