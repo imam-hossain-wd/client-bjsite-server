fb2e396989a3f69a15e60c0ecc394595eae4b020
imam-hossain-wd <zunayedemon6t9@gmail.com>
2024-06-23 22:53:06 +0600

    server complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcrypt");
 
 const port = process.env.PROT || 5000;
 const SALT_ROUND = Number(process.env.BCRYPT_SALT_ROUND);
@@ -26,56 +26,108 @@ const client = new MongoClient(uri, {
 const run = async () => {
 
     const userCollection = client.db("bjwala").collection("user");
+    const homepageCollection = client.db("bjwala").collection("homepage");
 
     try {
 
         // user operation
 
+        // app.post("/user", async (req, res) => {
+        //     try {
+        //         const user = req.body;
+        //         console.log(user, 'user');
+        //         const userId = user?.user_id;
+        //         const existingUser = await userCollection.findOne({ user_id: userId });
+
+        //         if (existingUser && existingUser.role === user.role) {
+        //             return res.status(400).send({ error: "User already exists" });
+        //         }
+
+        //         if (user.password) {
+        //             user.password = await bcrypt.hash(user.password, SALT_ROUND);
+        //         }
+
+        //         const result = await userCollection.insertOne(user);
+        //         console.log(result, 'result');
+        //         res.send(result);
+        //     } catch (error) {
+        //         console.error(error);
+        //         res.status(500).send({ error: "An error occurred while creating the user" });
+        //     }
+        // });
+
         app.post("/user", async (req, res) => {
             try {
                 const user = req.body;
+                console.log(user, 'user');
                 const userId = user?.user_id;
                 const existingUser = await userCollection.findOne({ user_id: userId });
-
+        
                 if (existingUser) {
-                    return res.status(400).send({ error: "User already exists" });
+                    const sameRoleUser = await userCollection.findOne({ user_id: userId, role: user.role });
+                    if (sameRoleUser) {
+                        return res.status(400).send({ error: "User with the same role already exists" });
+                    }
                 }
-
+        
                 if (user.password) {
-                    user.password = await bcrypt.hash(user.password, SALT_ROUND); // Hash the password if it exists
+                    user.password = await bcrypt.hash(user.password, SALT_ROUND); 
                 }
-
+                
                 const result = await userCollection.insertOne(user);
+                console.log(result, 'result');
                 res.send(result);
             } catch (error) {
                 console.error(error);
                 res.status(500).send({ error: "An error occurred while creating the user" });
             }
         });
+        
 
 
         app.post("/login", async (req, res) => {
             try {
                 const { email, password } = req.body;
-                const user = await userCollection.findOne({ email });
+                console.log(email, 'email');
 
+                // Find the user by email
+                const user = await userCollection.findOne({ email });
+                console.log(user, 'user');
+                console.log(email, password, 'email, password');
+
+                // If the user is not found, return an error
                 if (!user) {
                     return res.status(400).send({ error: "Invalid email or password" });
                 }
 
+                // Compare the provided password with the hashed password in the database
                 const isPasswordValid = await bcrypt.compare(password, user.password);
 
+                // If the password is invalid, return an error
                 if (!isPasswordValid) {
                     return res.status(400).send({ error: "Invalid email or password" });
                 }
 
+                // Create a JWT token
                 const token = jwt.sign(
                     { userId: user._id, email: user.email },
                     process.env.JWT_SECRET,
                     { expiresIn: '1h' }
                 );
+                console.log(token, 'token');
 
-                res.send({ message: "Login successfully", token });
+                // Send the response with the token and user information
+                res.send({
+                    message: "Login successfully",
+                    token,
+                    user: {
+                        id: user._id,
+                        email: user.email,
+                        name: user.name,
+                        phone: user.phone,
+                        role: user.role
+                    }
+                });
             } catch (error) {
                 console.error(error);
                 res.status(500).send({ error: "An error occurred during login" });
@@ -94,7 +146,7 @@ const run = async () => {
                     query.role = role;
                 }
 
-                const users = await userCollection.find(query).skip(skip).limit(limit).toArray();
+                const users = await userCollection.find(query).skip(skip).sort({ user_id: 1 }).limit(limit).toArray();
                 const totalUsers = await userCollection.countDocuments(query);
 
                 const response = {
@@ -132,21 +184,22 @@ const run = async () => {
 
         // find by agent id 
 
-        app.get("/user/agent/:id", async (req, res) => {
+        app.get("/user/agent/:id/:role", async (req, res) => {
             try {
                 const id = req.params.id;
-                const query = { user_id: id };
+                const role = req.params.role;
+                const query = { user_id: id , role:role};
                 const result = await userCollection.findOne(query);
 
                 const message = {
-                    message:'আপনি যে এজেন্ট খুজচ্ছেন তার নাম আমাদের লিষ্টে নেই |দয়া করে কাষ্টমার সার্ভিসে যোগাযোগ করুন।',
-                    helpline:"কাষ্টমার সার্ভিস এর নাম্বার গুলো পেতে এই লিঙ্ক এ ক্লিক করুন"
+                    message: 'আপনি যে এজেন্ট খুজচ্ছেন তার নাম আমাদের লিষ্টে নেই |দয়া করে কাষ্টমার সার্ভিসে যোগাযোগ করুন।',
+                    helpline: "কাষ্টমার সার্ভিস এর নাম্বার গুলো পেতে এই লিঙ্ক এ ক্লিক করুন"
                 }
-        
+
                 if (!result) {
                     return res.status(404).send({ message });
                 }
-        
+
                 res.send(result);
             } catch (error) {
                 console.error(error);
@@ -154,28 +207,28 @@ const run = async () => {
             }
         });
 
-        
+
 
         app.get("/user/:id", async (req, res) => {
             try {
                 const id = req.params.id;
-                
+
                 // Validate ObjectId
                 if (!ObjectId.isValid(id)) {
                     throw new Error("Invalid user ID");
                 }
-        
+
                 const query = { _id: new ObjectId(id) };
                 const result = await userCollection.findOne(query);
-        
+
                 if (!result) {
                     throw new Error("User not found");
                 }
-        
+
                 res.send(result);
             } catch (error) {
                 console.error(error);
-                
+
                 if (error.message === "User not found") {
                     res.status(404).send({ error: error.message });
                 } else if (error.message === "Invalid user ID") {
@@ -193,17 +246,17 @@ const run = async () => {
             const updatedUser = req.body;
             console.log(updatedUser, 'updatedUser');
             console.log(id, 'id');
-        
+
             // Validate ObjectId
             if (!ObjectId.isValid(id)) {
                 return res.status(400).send({ error: 'Invalid user ID' });
             }
-        
+
             const query = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: updatedUser,
             };
-        
+
             try {
                 const result = await userCollection.updateOne(query, updateDoc);
                 res.send(result);
@@ -220,6 +273,84 @@ const run = async () => {
             console.log(result, 'result');
             res.send(result);
         });
+
+
+
+        app.post("/homepage", async (req, res) => {
+            try {
+                const { customer_service_banner, master_agent_banner } = req.body;
+
+                // Validate input
+                if (!customer_service_banner || !master_agent_banner) {
+                    return res.status(400).send({ error: "Both banners are required" });
+                }
+
+                const result = await homepageCollection.insertOne({
+                    customer_service_banner,
+                    master_agent_banner,
+                    createdAt: new Date(),
+                });
+
+                res.send(result);
+            } catch (error) {
+                console.error("Error adding homepage banners:", error);
+                res.status(500).send({ error: "An error occurred while adding homepage banners" });
+            }
+        });
+
+
+        // PATCH route to update an existing banner
+        app.patch("/homepage/:id", async (req, res) => {
+            try {
+                const id = req.params.id;
+                const { customer_service_banner, master_agent_banner } = req.body;
+
+                // Validate ObjectId
+                if (!ObjectId.isValid(id)) {
+                    return res.status(400).send({ error: "Invalid ID" });
+                }
+
+                const query = { _id: new ObjectId(id) };
+                const updateDoc = {
+                    $set: {
+                        ...(customer_service_banner && { customer_service_banner }),
+                        ...(master_agent_banner && { master_agent_banner }),
+                        updatedAt: new Date(),
+                    },
+                };
+
+                const result = await homepageCollection.updateOne(query, updateDoc);
+
+                if (result.matchedCount === 0) {
+                    return res.status(404).send({ error: "Banner not found" });
+                }
+
+                res.send(result);
+            } catch (error) {
+                console.error("Error updating homepage banner:", error);
+                res.status(500).send({ error: "An error occurred while updating homepage banner" });
+            }
+        });
+
+        app.get("/homepage", async (req, res) => {
+            try {
+                const query = {};
+                const homepageBanner = await homepageCollection.findOne(query);
+
+                if (!homepageBanner) {
+                    return res.status(404).send({ error: "Homepage banner not found" });
+                }
+
+                res.send(homepageBanner);
+            } catch (error) {
+                console.error("Error fetching homepage banner:", error);
+                res.status(500).send({ error: "An error occurred while fetching homepage banner" });
+            }
+        });
+
+
+
+
     } finally {
     }
 };
